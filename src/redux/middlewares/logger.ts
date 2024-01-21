25567ac49fe071d0941816292a3cfa299c662581
MD MONIRUZZAMAN <96796294+Moniruzzaman2525@users.noreply.github.com>
2024-01-21 22:25:28 +0600

    challenge - 1 done

--- src/redux/middlewares/logger.ts
+++ src/redux/middlewares/logger.ts
@@ -1,11 +1,11 @@
 import { Middleware } from "@reduxjs/toolkit";
 
 const logger: Middleware = (store) => (next) => (action) => {
-    console.log('current state',store.getState());
-    console.log('action',action);
+    console.log('current state: ',store.getState());
+    console.log('action: ',action);
 
     next(action)
-    console.log('update state',store.getState());
+    console.log('update state: ',store.getState());
 }
 
 export default logger
